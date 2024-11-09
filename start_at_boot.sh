cbc007221c19af8ec584d69ca0d9eaf374256eaf
ParagPat20 <parag.pat20@gmail.com>
2024-11-09 17:08:05 +0530

    sleepadded

--- start_at_boot.sh
+++ start_at_boot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd ~/new-repo  # Change to your app directory
+sleep 10  # Wait for 10 seconds
+cd /path/to/your/app  # Change to your app directory
 python3 app1.py  # Or the command to start your app
