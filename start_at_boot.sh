7339bc9f0195315104a041b579032270660d5886
ParagPat20 <parag.pat20@gmail.com>
2024-11-09 18:46:11 +0530

    fe

--- start_at_boot.sh
+++ start_at_boot.sh
@@ -12,17 +12,13 @@ while true; do
     # Check if the display is connected
     if xrandr | grep " connected"; then
         echo "Display is connected. Terminating loading screen and launching the Python application in tmux..."
-
-        # Kill the loading screen
-        kill $LOADING_PID
-
-        # Start a new tmux session named 'myapp' and run the Python application
         tmux new-session -d -s myapp 'cd /home/jecon/new-repo && python3 app1.py'
-
+        sleep 3
+        kill $LOADING_PID
         echo "Python application is running in tmux session 'myapp'."
         break
     else
-        echo "Display not connected. Checking again in 5 seconds..."
-        sleep 5
+        echo "Display not connected. Checking again in 1 seconds..."
+        sleep 1
     fi
 done
