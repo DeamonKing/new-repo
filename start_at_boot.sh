9505a6104112416a6269031bc18c27136162956c
ParagPat20 <parag.pat20@gmail.com>
2024-11-09 17:09:06 +0530

    dae

--- start_at_boot.sh
+++ start_at_boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 10  # Wait for 10 seconds
-cd /path/to/your/app  # Change to your app directory
+cd /home/jecon/new-repo  # Change to your app directory
 python3 app1.py  # Or the command to start your app
